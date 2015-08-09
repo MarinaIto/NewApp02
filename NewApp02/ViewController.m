d9b9bde927ce289c1b8c3af02c75883c2505b85d
Marina Ito <marina_ito@toki.waseda.jp>
2015-08-09 14:07:02 +0800

    to tomo

--- NewApp02/ViewController.m
+++ NewApp02/ViewController.m
@@ -7,10 +7,10 @@
 //
 
 #import "ViewController.h"
-typedef NS_ENUM(NSUInteger, Class){
-    cars = 0,
-    watches,
-};
+//typedef NS_ENUM(NSUInteger, Class){
+//    cars = 0,
+//    watches,
+//};
 
 @interface ViewController ()
 @property(nonatomic) NSArray *cars;
@@ -34,17 +34,30 @@ typedef NS_ENUM(NSUInteger, Class){
     NSString *path = [bundle pathForResource:@"Property List" ofType:@"plist"];
     //プロパティリストの中身データを取得
     NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:path];
-//    NSArray *cars = [dic objectForKey:@"cars"];
+    
+    NSLog(@"dic = %@",dic);
+    NSArray *cars = [dic objectForKey:@"cars"];
+    
+    NSLog(@"cars = %@",cars);
+    NSLog(@"cars[0] = %@",cars[0]);
+    NSLog(@"cars[0][3] = %@",cars[0][3]);
+
+    
+    
+    
     NSArray *watches = [dic objectForKey:@"watches"];
+//    NSLog(@"watchs = %@",watches);
     
     //取得できた配列データをメンバ変数に代入
-//    self.cars = cars;
+    self.cars = cars;
     self.watches = watches;
     
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-        return 2;
+//        return 2;
+    return _cars.count;
+    
 }
     
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
@@ -54,9 +67,22 @@ typedef NS_ENUM(NSUInteger, Class){
         
         // 表示しているセクションのタイトルを
         switch (section) {
-            case watches:
-                title = @"時計";
+            case 0:
+                title = @"その他";
                 break;
+            case 1:
+                title = @"イギリス車";
+                break;
+            case 2:
+                title = @"ドイツ車";
+                break;
+            case 3:
+                title = @"イタリア車";
+                break;
+            case 4:
+                title = @"日本車";
+                break;
+                
             default:
                 break;
         }
@@ -67,10 +93,33 @@ typedef NS_ENUM(NSUInteger, Class){
     
     //データの数によって行数が増える。
     NSInteger rows;
+//    switch (section) {
+//            case cars:
+//            rows = [self.cars count];
+//            break;
+//        case watches:
+//            rows = [self.watches count];
+//            break;
+//        default:
+//            break;
+//    }
     switch (section) {
-        case watches:
-            rows = [self.watches count];
+        case 0:
+            rows = [_cars[0] count];
             break;
+        case 1:
+            rows = [_cars[1] count];
+            break;
+        case 2:
+            rows = [_cars[2] count];
+            break;
+        case 3:
+            rows = [_cars[3] count];
+            break;
+        case 4:
+            rows = [_cars[4] count];
+            break;
+            
         default:
             break;
     }
@@ -86,10 +135,38 @@ typedef NS_ENUM(NSUInteger, Class){
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     NSString *itemName;
+//    switch (indexPath.section) {
+//              case 0:
+//            itemName = self.cars[indexPath.row];
+//              case watches:
+//            itemName = self.watches[indexPath.row];
+//            break;
+//        default:
+//            break;
+//    }
+    
     switch (indexPath.section) {
-              case watches:
-            itemName = self.watches[indexPath.row];
+        case 0:
+            itemName = self.cars[0][indexPath.row];
             break;
+        case 1:
+            itemName = self.cars[1][indexPath.row];
+            break;
+        case 2:
+            itemName = self.cars[2][indexPath.row];
+            break;
+        case 3:
+            itemName = self.cars[3][indexPath.row];
+            break;
+        case 4:
+            itemName = self.cars[4][indexPath.row];
+            break;
+
+
+//
+//        case watches:
+//            itemName = self.watches[indexPath.row];
+//            break;
         default:
             break;
     }
